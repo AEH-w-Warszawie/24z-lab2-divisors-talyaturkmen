efcbb096f2e82a51a44b6f415b7cec457a7d4364
Aynur Talya Turkmen <turkmentalya@gmail.com>
2024-12-06 14:16:35 +0100

    task2 divisors optimization

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -4,10 +4,18 @@
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
 	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
+	for (unsigned long long i = 1; i * i <= number; ++i)
 	{
-		if (number % i == 0)
-			result.push_back(i);
+		if (number % i == 0) // ensure i is divisor
+		{
+			result.push_back(i); // push back i since its a divisor
+
+			if (i != number / i)
+			{
+				result.push_back(number / i); // Making sure that  i is not the square root of number
+			}
+		}
 	}
+
 	return result;
 }
